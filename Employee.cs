4a34164c95f3a6d97549a4b1da53fe8a1471008a
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-03-25 23:27:00 +0200

    HW2403_SQLiteWithEmployeeClass

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqLite240319
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(int id, string name, int age, string address, int salary)
+        {
+            this.id = id;
+            Name = name;
+            Age = age;
+            Address = address;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee id: {id}, Name: {Name}, Age: {Age}, Address: {Address} , Salary: {Salary}.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqLite240319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            SQLiteConnection connection = new SQLiteConnection($"Data Source = D:\\HackerU\\SQL_Example.db; Version = 3;");
+            connection.Open();
+            using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM COMPANY",connection))
+            {
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        employees.Add(new Employee(Convert.ToInt32(reader.GetValue(0)), reader.GetValue(1).ToString(), 
+                            Convert.ToInt32(reader.GetValue(2)), reader.GetValue(3).ToString(), Convert.ToInt32(reader.GetValue(4))));
+                        Console.WriteLine($"{reader.GetValue(0), 10} {reader.GetValue(1),10} {reader.GetValue(2),10} {reader.GetValue(3),13} {reader.GetValue(4),10}");
+                    }
+                }
+            }
+
+            connection.Close();
+            Console.WriteLine("==========The List: ===============");
+            employees.ForEach(n => Console.WriteLine(n));
+
+            Console.ReadLine();
+        }
+    }
+}
